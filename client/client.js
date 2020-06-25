a2f92b0a114cf813539ff5e822ea89745b9068ce
AnikeyPro <xandreyx2@gmail.com>
2020-06-25 03:22:07 +0300

    working invite

--- client/client.js
+++ client/client.js
@@ -1,72 +1,87 @@
-//конектимся к серверу
-const sock = io({ transports: ['websocket'], upgrade: false });
-
 const app = new Vue({
     el: '#app',
     data: {
-        users: [],
-        messages: ['Game Log']
+        user: "",
+        opponent: "",
+        usersOnline: [],
+        messages: ['Game Log'],
+        showInv: false
     },
     methods: {
         resetUsers: function (arr) {
-            this.users = arr;
+            this.usersOnline = arr;
         },
         updateMessage: function (msg) {
             this.messages.push(msg);
         },
         sendInv: function (target) {
-            const sender = $('#username').text();
-            console.log([target, sender]);
+            console.log(this.user, target);
             //отправляем приглос
-            sock.emit('sendRequest', [target, sender]);
-
+            socket.emit('sendRequest', { 'from': this.user, 'to': target, });
+        },
+        createInvitation: function (opponent) {
+            this.opponent = opponent;
+            this.showInv = true;
+        },
+        declineInv: function () {
+            this.showInv = false;
         }
     }
 
 });
+//конектимся к серверу
+const socket = io.connect();
 
-
-sock.on("connect", () => {
-    var user = $('#username').text();
-
+socket.on("connect", () => {
     console.log('connected!');
 
-    sock.emit('login', user);
+    //получаем юзера
+    socket.on('username', (username) => {
+        app.user = username;
+    })
 
     //юзеры - онлайн
-    sock.on('data', (data) => {
+    socket.on('data', (data) => {
         app.resetUsers(data);
     });
 
     //лог игры
-    sock.on('message', (msg) => {
+    socket.on('message', (msg) => {
         app.updateMessage(msg);
     });
 
-
     //получаем приглос
-    sock.on('invitation', (arr) => {
-        console.log(arr[0] + 'priglos');
-        if (arr[0] === user) {
-            alert("You are invited to play by  " + arr[1]);
+    socket.on('invitation', (req) => {
+        console.log(req.from + ' kinul priglos' + app.user);
+        if (req.to === app.user) {
+            app.createInvitation(req.from);
         }
     });
 
 
-});
+    // socket.on('disconnect', function () {
+    //     socket.disconnect();
+    //     setTimeout(function () {
+    //         console.log("client trying reconnect");
+    //         io.connect('localhost:8080'{'forceNew':true });
+    //     }, 2000);
+    // });
 
+});
 
 
 
 
 $(document).ready(function () {
+
+
     //обработчик игры 
     const addButtonListeners = () => {
         ['rock', 'paper', 'scissors', 'lizard', 'spok'].forEach((id) => {
             const button = document.getElementById(id);
             button.addEventListener('click', () => {
                 console.log("Clicked" + id)
-                sock.emit('turn', id);
+                socket.emit('turn', id);
             });
         });
     };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+  require('dotenv').config()
 }
-  
+
 
 const express = require('express');
 const app = express();
@@ -32,19 +32,19 @@ db.serialize(() => {
     );
   } else {
     console.log('Database ready to go!');
-    
+
   }
 });
 
 
 //список онлайн
-let usersOnline = [];
-
+var usersOnline = {};
+var user = null;
 
 //работаем с авторизацией через passport
 const passport = require('passport');
 const initializePassport = require('./passport-config')
-initializePassport(passport,db);
+initializePassport(passport, db);
 
 
 
@@ -52,134 +52,145 @@ initializePassport(passport,db);
 const bcrypt = require('bcrypt');
 
 //применеяем ejs 
-app.set('view-engine','ejs');
-app.use(express.urlencoded({ extended: false}));
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/client'));
 app.use(flash());
 app.use(session({
-    store: new SQLiteStore,
-    secret: process.env.SESSION_SECRET,
-    resave:false,
-    saveUninitialized: false,
-    cookie: { maxAge: 60 * 60 * 1000 }
+  store: new SQLiteStore,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60 * 60 * 1000 }
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride('_method'));
 
 
-app.get('/', checkAuthenticated, (req,res) => {
-
-    res.render('index.ejs',{username:req.user.username});
-
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { username: req.user.username });
+  user = req.user.username;
 });
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-  })
-  
+  res.render('login.ejs')
+})
+
 app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
 }))
 
-app.get('/register', checkNotAuthenticated, (req,res) => {
-    res.render('registration.ejs');
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('registration.ejs');
 });
 
-app.post('/register', checkNotAuthenticated, async (req,res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const clearUsername = cleanseString(req.body.username);
-        console.log(hashedPassword,clearUsername);  
-        db.run(`INSERT INTO Users (username, password) VALUES ("${clearUsername}","${hashedPassword}")`);
-        res.redirect('/login');
-    } catch  {
-        res.redirect('/register');
-    }
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const clearUsername = cleanseString(req.body.username);
+    console.log(hashedPassword, clearUsername);
+    db.run(`INSERT INTO Users (username, password) VALUES ("${clearUsername}","${hashedPassword}")`);
+    res.redirect('/login?registered');
+  } catch  {
+    res.redirect('/register');
+  }
 });
 
 
 app.delete('/logout', (req, res) => {
-    // if (usersOnline.indexOf(req.user.username) > 0){
-    //   usersOnline.splice(usersOnline.indexOf(req.user.username));
-    // }
-    req.logOut()
-    res.redirect('/login')
-  })
+  req.logOut()
+  req.session.destroy(function (err) {
+    res.redirect('/login'); //Inside a callback… bulletproof!
+  });
+})
 
 
 
 //проверка на аутентификацию
 function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-    res.redirect('/login')
+  if (req.isAuthenticated()) {
+    return next()
   }
-  
-  function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return res.redirect('/')
-    }
-    next()
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
   }
+  next()
+}
 
 
 //очистка строки от спецсимволов
-  const cleanseString = function(string) {
-    return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-  };
-  
+const cleanseString = function (string) {
+  return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+};
+
 
 
 let requestedPalyer = null;
 
-// проверяем на коннект с сервером
-io.on('connection',(sock)=>{
-    console.log('Someone connected... emmm hi there ' + usersOnline)
-    var username;
 
-  
-    //при логине даем всем знать что залогинились
-    sock.on('login', (userTokenOrId) => {
-      username = userTokenOrId;
-      console.log('we are loggin in ' + userTokenOrId)
-      if(!usersOnline.find(name => name === username ))
-        {usersOnline.push(username)}
-      console.log('alll  ' + usersOnline)
-      sock.emit('data', usersOnline);
-      sock.broadcast.emit('data', usersOnline);
+// проверяем на коннект с клиентом
+io.on('connection', (sock) => {
+  console.log('Someone connected... emmm hi there ')
 
-    });
-    //логаут - убираем из списка онлайн 
-    sock.on('disconnect', (userTokenOrId) => {
-      if (usersOnline.indexOf(username) > 0){
-        usersOnline.splice(usersOnline.indexOf(username));
-      }
-      console.log('we are loggin out ' + username)
-      sock.broadcast.emit('data', usersOnline);
-    });
-    // перенаправляем приглос
-    sock.on('sendRequest', (arr) => {
-      console.log(arr[0] + 'priglos by serv' + arr[1]);
-      if (arr[1] === username){
-        sock.broadcast.emit('invitation', arr);
-      }
-
-    });
+//   setTimeout(function () {
+//     console.log("force disconnect");
+//     sock.disconnect();
+// }, 10000);
 
 
+
+  //отправляем логин
+  if (user != null) {
+    if (!Object.values(usersOnline).find(name => name === user)) { usersOnline[sock.id] = user; }
+    sock.emit('username', usersOnline[sock.id]);
+    console.log('i\'m still online ' + usersOnline[sock.id])
+    console.log('all users are  ' + Object.values(usersOnline))
+    sock.emit('data', Object.values(usersOnline));
+    sock.broadcast.emit('data', Object.values(usersOnline));
+  }
+
+
+  //логаут - убираем из списка онлайн 
+  sock.on('disconnect', (reason) => {
+    console.log('reason' + reason);
+    console.log('i\'m loggin out ' + usersOnline[sock.id])
+    delete usersOnline[sock.id];
+    sock.broadcast.emit('data', Object.values(usersOnline));
+    console.log('users after logout  ' + Object.values(usersOnline))
+  });
+
+
+  // перенаправляем приглос
+  sock.on('sendRequest', (req) => {
+    console.log('username' + user);
+    console.log(req.from + ' sends to ' + req.to);
+    console.log(req.to + ' emmit ' + user);
+    if (req.from === usersOnline[sock.id]) {
+      sock.broadcast.emit('invitation', req);
+    }
+
+  });
+
+
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  console.log('==================' + time);
 
 });
 
 
-server.on('error',(err)=>{
-    console.log('Server error', err)
+server.on('error', (err) => {
+  console.log('Server error', err)
 })
 
-server.listen(8080,()=>{
-    console.log('started on 8080')
+server.listen(8080, () => {
+  console.log('started on 8080')
 })
 
