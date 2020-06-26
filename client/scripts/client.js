d85ca5ab0c9bb73b4a230f8396524f1b7bf42ee0
AnikeyPro <xandreyx2@gmail.com>
2020-06-26 22:16:49 +0300

    Интерфейс для игры, чат и прочие доработки.

---
+++ client/scripts/client.js
@@ -0,0 +1,125 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        user: userName.toString(),
+        opponent: "",
+        usersOnline: [],
+        messages: ['Game Log'],
+        showInv: false,
+        roomName: '',
+        gameOn: false,
+        msg:''
+    },
+    methods: {
+        resetUsers: function (arr) {
+            this.usersOnline = arr;
+        },
+        sendInv: function (target) {
+            console.log(this.user, target);
+            this.roomName = target + '&' + this.user  ;
+            //отправляем приглос
+            this.opponent = target;
+            socket.emit('send-request', { 'from': this.user, 'to': target, });
+        },
+        createInvitation: function (opponent) {
+            this.roomName = this.user + '&' + opponent;
+            this.opponent = opponent;
+            this.showInv = true;
+        },
+        declineInv: function () {
+            this.showInv = false;
+        },
+        startGame: function () {
+            this.gameOn = true;
+            localStorage.gameOn = true;
+            if (this.showInv){
+                this.showInv = false;
+                socket.emit('agreed-to-play', { 'from': this.user, 'to': this.opponent });
+            }
+        },
+        leaveGame: function () {
+            console.log('ЖМИ');
+            this.gameOn = false;
+            localStorage.gameOn = false;
+        },
+        sendToChat: function (e){
+            e.preventDefault();
+            this.messages.push('You : ' + this.msg);
+            socket.emit('message', this.roomName, this.user + " : " + this.msg);
+            this.msg = '';
+        }
+
+    },
+    mounted() {
+      if (localStorage.gameOn) {
+        this.gameOn = JSON.parse(localStorage.gameOn);
+      }
+    }
+
+});
+//конектимся к серверу
+const socket = io.connect();
+
+socket.on("connect", () => {
+    console.log('connected!');
+
+    //юзеры - онлайн
+    socket.on('users-online', (usersOnline) => {
+        app.resetUsers(usersOnline);
+    });
+
+    //получаем приглос
+    socket.on('invitation', (req) => {
+        console.log(req.from + ' kinul priglos' + app.user);
+        app.opponent = req.from;
+        if (req.to === app.user) {
+            app.createInvitation(req.from);
+        }
+    });
+
+    socket.on('message', (msg) => {
+        app.messages.push(msg);
+    });
+    socket.on('lets-play', (req, room) => {
+        console.log(req.from + ' agreed lets play' + app.user);
+        if (req.to === app.user) {
+            app.startGame();
+            socket.emit('connect-me-too', room, app.user);
+        }
+    });
+
+
+    // socket.on('disconnect', function () {
+    //     socket.disconnect();
+    //     console.log('disconnected');
+    //     location.reload();
+
+    // });
+
+});
+
+
+
+
+// $(document).ready(function () {
+
+
+//     //обработчик игры 
+//     const addButtonListeners = () => {
+//         ['rock', 'paper', 'scissors', 'lizard', 'spok'].forEach((id) => {
+//             const button = document.getElementById(id);
+//             button.addEventListener('click', () => {
+//                 console.log("Clicked" + id)
+//                 socket.emit('turn', id);
+//             });
+//         });
+//     };
+
+//     addButtonListeners();
+
+
+
+// });
+
+
+
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ db.serialize(() => {
   console.log(exists);
   if (!exists) {
     db.run(
-      "CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
+      "CREATE TABLE Users (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, password TEXT)"
     );
   } else {
     console.log('Database ready to go!');
@@ -36,17 +36,10 @@ db.serialize(() => {
   }
 });
 
-
-//список онлайн
-var usersOnline = {};
-var user = null;
-
 //работаем с авторизацией через passport
 const passport = require('passport');
 const initializePassport = require('./passport-config')
 initializePassport(passport, db);
-
-
 
 //для шифровки паролей использую bcrypt
 const bcrypt = require('bcrypt');
@@ -68,9 +61,17 @@ app.use(passport.session());
 app.use(methodOverride('_method'));
 
 
+//список онлайн
+const usersOnline = {};
+var username = null;
+
+//комнаты
+const rooms = { }
+
+
 app.get('/', checkAuthenticated, (req, res) => {
   res.render('index.ejs', { username: req.user.username });
-  user = req.user.username;
+  username = req.user.username;
 });
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
@@ -103,80 +104,91 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
 app.delete('/logout', (req, res) => {
   req.logOut()
   req.session.destroy(function (err) {
-    res.redirect('/login'); //Inside a callback… bulletproof!
+    res.redirect('/login'); 
   });
 })
 
 
-
-//проверка на аутентификацию
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next()
-  }
-  res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/')
-  }
-  next()
-}
-
-
-//очистка строки от спецсимволов
-const cleanseString = function (string) {
-  return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-};
-
-
-
-let requestedPalyer = null;
-
-
 // проверяем на коннект с клиентом
-io.on('connection', (sock) => {
+io.on('connection', (socket) => {
   console.log('Someone connected... emmm hi there ')
+  //   setTimeout(function () {
+  //     console.log("force disconnect");
+  //     sock.disconnect();
+  // }, 10000);
 
-//   setTimeout(function () {
-//     console.log("force disconnect");
-//     sock.disconnect();
-// }, 10000);
 
 
 
   //отправляем логин
-  if (user != null) {
-    if (!Object.values(usersOnline).find(name => name === user)) { usersOnline[sock.id] = user; }
-    sock.emit('username', usersOnline[sock.id]);
-    console.log('i\'m still online ' + usersOnline[sock.id])
+  if (username != null) {
+    if (!Object.values(usersOnline).find(name => name === username)) { usersOnline[socket.id] = username; }
+    console.log('i\'m still online ' + usersOnline[socket.id] + "sock "+ socket.id)
     console.log('all users are  ' + Object.values(usersOnline))
-    sock.emit('data', Object.values(usersOnline));
-    sock.broadcast.emit('data', Object.values(usersOnline));
+
+    //отправляем список юзеров онлайн
+    socket.emit('users-online', Object.values(usersOnline));
+    socket.broadcast.emit('users-online', Object.values(usersOnline));
   }
 
 
   //логаут - убираем из списка онлайн 
-  sock.on('disconnect', (reason) => {
-    console.log('reason' + reason);
-    console.log('i\'m loggin out ' + usersOnline[sock.id])
-    delete usersOnline[sock.id];
-    sock.broadcast.emit('data', Object.values(usersOnline));
+  socket.on('disconnect', (reason) => {
+    console.log('reason ' + reason);
+    console.log('i\'m loggin out ' + usersOnline[socket.id])
+    delete usersOnline[socket.id];
+    // getUserRooms(socket).forEach(room => {
+    //   console.log('puk ' + room);
+    //   socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+    //   console.log('deleting room ' + room);
+    //   delete rooms[room];
+    // })
+    socket.broadcast.emit('users-online', Object.values(usersOnline));
     console.log('users after logout  ' + Object.values(usersOnline))
   });
 
 
   // перенаправляем приглос
-  sock.on('sendRequest', (req) => {
-    console.log('username' + user);
+  socket.on('send-request', (req) => {
+    console.log('username' + username);
     console.log(req.from + ' sends to ' + req.to);
-    console.log(req.to + ' emmit ' + user);
-    if (req.from === usersOnline[sock.id]) {
-      sock.broadcast.emit('invitation', req);
+    console.log(req.to + ' emmit ' + username);
+    if (req.from === usersOnline[socket.id]) {
+      socket.broadcast.emit('invitation', req);
     }
 
   });
+
+  //если согласился->создаем комнату для игры
+  socket.on('agreed-to-play', (req) => {
+    console.log('username' + username + ' = ' + req.from +' agreed to play with ' + req.to);
+    if (req.from === usersOnline[socket.id]) {
+      var room =  req.from + '&' + req.to;
+      console.log(req.from + 'user to room  ' + room);
+      socket.broadcast.emit('lets-play', req, room);
+      rooms[room] = { users: {} }
+      rooms[room].users[socket.id] = usersOnline[socket.id];
+      socket.join(room);
+    }
+  });
+
+  //коннектим пригласившего к комнате 
+  socket.on('connect-me-too', (room,username) => {
+    if (username === usersOnline[socket.id]) {
+      
+      console.log(username +' user to room ' + room);
+      rooms[room].users[socket.id] = usersOnline[socket.id];
+      socket.join(room);
+    }
+  });
+  
+
+  //чат
+  socket.on('message', (room,message) => {
+    console.log(JSON.stringify(socket.adapter.rooms) + 'aaa');
+    //console.log('users in room' + Object.entries(rooms[room].users));
+    socket.to(room).broadcast.emit('message', message);
+  })
 
 
   var today = new Date();
@@ -194,3 +206,33 @@ server.listen(8080, () => {
   console.log('started on 8080')
 })
 
+//проверка на аутентификацию
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+//очистка строки от спецсимволов
+const cleanseString = function (string) {
+  return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+};
+
+
+function getUserRooms(socket) {
+  console.log('come on delete this  ' + socket.id);
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    console.log('rooms are before' + names);
+    if (room.users[socket.id] != null) names.push(name)
+    console.log('rooms are ' + names);
+    return names
+  }, [])
+}
